f8773678d7930e824261246fd04257db50175886
kamrulcse404 <kamrul.h01960@gmail.com>
2022-10-21 16:40:18 +0600

    blog initial

---
+++ README.md
@@ -0,0 +1,1 @@
+Todo Blog with Laravel
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\ArticlesController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('articles', [ArticlesController::class, 'index']);
+
+Route::get('articles/create', [ArticlesController::class, 'create']);
+Route::post('articles', [ArticlesController::class, 'store']);
+
+Route::get('articles/{id}', [ArticlesController::class, 'show']);
+Route::put('articles/{id}', [ArticlesController::class, 'update']);
+Route::delete('articles/{id}', [ArticlesController::class, 'destroy']);
